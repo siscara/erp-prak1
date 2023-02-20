c95b9dff57489972c0517c0a00fe449420a8f653
siscara <siscadwirahayu05@gmail.com>
2023-02-20 14:44:49 +0700

    upload

---
+++ __manifest__.py
@@ -0,0 +1,26 @@
+{
+    "name"       : "New Addons",
+    "version"    : "1.0",
+    "author"     : "Sisca Dwi Rahayu",
+    "website"    : "https://www.detik.com/",
+    "category"   : "New Module",
+    "summary"    : "Ini adalah addons baru",
+    "description": """
+            Latihan membuat addons baru di odoo 1
+            """,
+    "depends" :[
+        "base",
+    ],
+    "data" :[
+
+    ],
+    "demo"      : [],
+    "test"      : [],
+    "image"     : [],
+    "qweb"      : [],
+    "css"       : [],
+   "application": True,
+   "insallable" : True,
+   "auto_install": False,
+
+}
